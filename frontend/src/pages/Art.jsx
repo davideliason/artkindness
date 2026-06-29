e2ce733729d81d757b57be7a7715b0b387547c4a
David Eliason <davethemaker@gmail.com>
2026-06-29 15:00:55 -0700

    add state using conditional for two fx

--- frontend/src/pages/Art.jsx
+++ frontend/src/pages/Art.jsx
@@ -1,38 +1,68 @@
-import { useState} from "react";
+import { useState } from "react";
 
-function Welcome() {
-  const [name,setName] = useState("Jane")
+function Welcome({name}){
+  const hellos = ["bonjour", "hola", "hello"];
+  const [ showGreetings, setShowGreetings ]=useState(false);
 
-  function sayHello() {
-    alert("hello" + name)
+  function sayHello(){
+     setShowGreetings(true);
   }
 
-   return (
+  function sayGoodbye(){
+    setShowGreetings(false)
+  }
+
+  return (
       <div>
-        <h3> Hello {name} </h3>
-        <button onClick={sayHello}> say hello </button>
+         <h3>hello, {name}</h3>
+         <button onClick={sayHello}>show</button>
+
+         <button onClick={sayGoodbye}>hide</button>
+
+          {showGreetings &&
+          hellos.map((helloo) => (
+              <p key={helloo}>{helloo}</p>
+            ))
+       }
+
       </div>
     )
 }
 
+function Counter(){
+  const [count,setCount] = useState(0)
 
+  function addOne(){
+    setCount(count +1);
+  }
+
+  function addOneRevised(){
+    setCount((currentCount) => currentCount +1 );
+  }
+
+  function subtractOne(){
+    setCount(count -1);
+  }
+
+  return (
+      <div>
+        <h3>bonjour {count}</h3>
+        <button onClick={addOne}>+</button>
+        <button onClick={subtractOne}>-</button>
+        <button onClick={addOneRevised}> New +</button>
+      </div>
+    );
+}
 
 export default function Art() {
   return (
     <section>
+      <Counter />
+      <Welcome name="John" />
       <h1>AI Art Generator</h1>
       <p>Upload a photo for an inspiring quote (coming soon).</p>
 
-     <Welcome name="hello" />
-
-
-
-      <input type="file" accept="image/*" />
-
-      <br />
-      <br />
-
-      <button>Generate Art</button>
+    
     </section>
   );
 }
