89bb30f7343cc23f98a151c3aa50540080b1c118
David Eliason <davethemaker@gmail.com>
2026-06-29 16:36:27 -0700

    refactor so there are name and email form inputs

--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -4,27 +4,37 @@ function NameForm(){
    const [name,setName] = useState("");
    const [email,setEmail] = useState("");
    const [submittedName,setSubmittedName]=useState("");
+   const [submittedEmail,setSubmittedEmail]=useState("");
 
    function handleNameChange(event){
       setName(event.target.value);
    }
 
+   function handleEmailChange(event){
+    setEmail(event.target.value);
+   }
+
    function handleSubmit(event){
     event.preventDefault();
     setSubmittedName(name);
+    setSubmittedEmail(email);
    }
 
    return (
       <div>
           <h5>Names</h5>
           <input value={name} onChange={handleNameChange}></input>
+          <input value={email} onChange={handleEmailChange}></input>
           <p>Hello {name} </p>
+          <p>Email: {email}</p>
 
           <form onSubmit={handleSubmit}>
             <input value={name} onChange={handleNameChange} />
+            <input value={email} onChange={handleEmailChange} />
             <button type="submit">Submit</button>
 
             <p>Submitted: {submittedName}</p>
+            <p>Submitted email: {submittedEmail}</p>
           </form>
       </div>
     )
