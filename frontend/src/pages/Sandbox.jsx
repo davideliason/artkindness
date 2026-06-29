0e023f4e305024e15692c202da81674c63e03e1d
David Eliason <davethemaker@gmail.com>
2026-06-29 15:28:00 -0700

    create function to set input name state

--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
+function NameForm(){
+   const [name,setName] = useState("");
+   const [submittedName,setSubmittedName]=useState("");
+
+   function handleChange(event){
+      setName(event.target.value);
+
+   }
+
+   return (
+      <div>
+          <h5>Names</h5>
+          <input value={name} onChange={handleChange}></input>
+          <p>Hello {name} </p>
+      </div>
+    )
+
+}
+
+
+
+
+
 export default function Sandbox() {
   return (
     <section>
       <h1>Sandbox</h1>
       <p>A place to try new things..</p>
+      <NameForm />
 
     </section>
   );
