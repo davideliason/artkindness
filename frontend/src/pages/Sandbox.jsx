89983826718a64b0936942b94763993d4502ac83
David Eliason <davethemaker@gmail.com>
2026-06-30 05:27:05 -0700

    add form for name and submitted name input values

--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -1,48 +1,28 @@
-import { useState } from "react";
+import {useState} from "react";
 
-function NameForm(){
-   const [name,setName] = useState("");
-   const [email,setEmail] = useState("");
-   const [submittedName,setSubmittedName]=useState("");
-   const [submittedEmail,setSubmittedEmail]=useState("");
+function FormData(){
+  const [name,setName] = useState("tom");
+  const [submittedName,setSubmittedName] = useState("");
 
-   function handleNameChange(event){
-      setName(event.target.value);
-   }
+  function handleChange(event){
+    setName(event.target.value);
+    }
 
-   function handleEmailChange(event){
-    setEmail(event.target.value);
-   }
+    function handleSubmit(event){
+      event.preventDefault();
+      setSubmittedName(name);
+    }
 
-   function handleSubmit(event){
-    event.preventDefault();
-    setSubmittedName(name);
-    setSubmittedEmail(email);
-   }
-
-   return (
-      <div>
-          <h5>Names</h5>
-          <input value={name} onChange={handleNameChange}></input>
-          <input value={email} onChange={handleEmailChange}></input>
-          <p>Hello {name} </p>
-          <p>Email: {email}</p>
-
-          <form onSubmit={handleSubmit}>
-            <input value={name} onChange={handleNameChange} />
-            <input value={email} onChange={handleEmailChange} />
-            <button type="submit">Submit</button>
-
-            <p>Submitted: {submittedName}</p>
-            <p>Submitted email: {submittedEmail}</p>
-          </form>
-      </div>
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={name} onChange={handleChange} />
+        <button type="submit">submit</button>
+        <h5>submitted name: {submittedName}</h5>
+      </form>
+    </div>
     )
-
 }
-
-
-
 
 
 export default function Sandbox() {
@@ -50,7 +30,7 @@ export default function Sandbox() {
     <section>
       <h1>Sandbox</h1>
       <p>A place to try new things..</p>
-      <NameForm />
+      <FormData />
 
     </section>
   );
