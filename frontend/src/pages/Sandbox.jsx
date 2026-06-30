9386c242a96dc96ebffcb99635c05c393f38edd4
David Eliason <davethemaker@gmail.com>
2026-06-30 08:35:05 -0700

    add input for new todo and then list all todos

--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -1,5 +1,44 @@
 import {useState} from "react";
 
+function TodoList(){
+
+  const [todos, setTodos] = useState([
+     { id:1, text:"blue"},
+     { id:2, text:"red" }
+  ]);
+
+  const [todoText,setTodoText] = useState("");
+
+  function handleTodoTextChange(event){
+    setTodoText(event.target.value);
+  }
+
+  function addTodo(){
+    const newTodo = {
+      id: Date.now(),
+      text: todoText
+    };
+
+    setTodos([...todos,newTodo]);
+    setTodoText("");
+  }
+
+  return (
+     <div>
+        <h5>ToDos</h5>
+        <input value={todoText} onChange={handleTodoTextChange} />
+        <button onClick={addTodo}>Add todo</button>
+        <ul>
+          <li>
+            {todos.map((todo) => 
+                <li key={todo.id}>{todo.text}</li>
+              )}
+          </li>
+        </ul>
+     </div>
+    );
+}
+
 function SignUpForm(){
   const [formData,setFormData]=useState(
    {
@@ -31,8 +70,10 @@ export default function Sandbox() {
   return (
     <section>
       <h1>Sandbox</h1>
-      <p>A place to try new things..</p>
+      <p>A place to try new things here..</p>
       <SignUpForm />
+      <TodoList />
+    
 
     </section>
   );
