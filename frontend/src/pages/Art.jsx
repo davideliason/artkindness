94e53de0c7a64eb06f8f833359c9f754b98c60e1
David Eliason <davethemaker@gmail.com>
2026-06-30 11:45:48 -0700

    fix art page

--- frontend/src/pages/Art.jsx
+++ frontend/src/pages/Art.jsx
@@ -1,15 +1,9 @@
 
-
-
-
 export default function Art() {
   return (
     <section>
       <h1>AI Art Generator</h1>
       <p>Upload a photo for an inspiring quote (coming soon).</p>
-       <button onClick={callLambda}>Call Lambda</button>
-      <h5>The Lambda message is: </h5>
-      <p>{message}</p>
     
     </section>
   );
