644d97eb29a18623f36bebebbc669d080a60e037
David Eliason <davethemaker@gmail.com>
2026-06-30 13:49:47 -0700

    add post route successfully which works with previous get

--- frontend/src/pages/Sandbox.jsx
+++ frontend/src/pages/Sandbox.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 export default function Sandbox() {
-
+  const [artworkName, setArtworkName] = useState("Kind Robot");
   const [message, setMessage] = useState("");
+  const [returnedName, setReturnedName] = useState("test");
 
   async function callLambda() {
     const response = await fetch(
@@ -11,18 +12,47 @@ export default function Sandbox() {
 
     const data = await response.json();
 
+    console.log(data);
+
     setMessage(data.message);
   }
-  
+
+  async function sendArtworkName() {
+    const response = await fetch(
+      "https://jn41btrinc.execute-api.us-west-2.amazonaws.com/artwork",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          artworkName: artworkName,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setReturnedName(data.artworkName);
+  }
+
   return (
     <section>
       <h1>Sandbox</h1>
-      <p>A place to try new things here..</p>
+      <p>A place to try new things here.</p>
 
-         <button onClick={callLambda}>Call Lambda</button>
+      <button onClick={callLambda}>Call Hello Lambda</button>
       <p>{message}</p>
-    </section>
 
-   
+      <input
+        value={artworkName}
+        onChange={(event) => setArtworkName(event.target.value)}
+      />
+
+      <button onClick={sendArtworkName}>Send Artwork Name</button>
+      <p>Returned name: {returnedName}</p>
+    </section>
   );
 }
